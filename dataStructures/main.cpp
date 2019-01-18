a923ce3f293f963a1f9202dd7fba599d75a1b3d3
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-01-18 19:13:01 +0530

    Data structures Problem Input taken

---
+++ dataStructures/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    //Start taking inputs
+    int num_test_cases =0;
+    vector< vector<int> > input;
+    while(true){
+        cin>>num_test_cases;
+        if(num_test_cases==0)
+            break;
+        vector<int> no_workshop;
+        no_workshop.push_back(num_test_cases);
+        input.push_back(no_workshop);
+        for(int i=0;i<num_test_cases;i++){
+            vector<int> temp;
+            int num_elements;
+            cin>>num_elements;
+            temp.push_back(num_elements);
+            for(int j=0;j<num_elements;j++){
+                int cost_price;
+                cin>>cost_price;
+                temp.push_back(cost_price-10);
+            }
+            input.push_back(temp);
+        }
+    }
+    //Input Done
+    int first_dimension_size = input.size();
+
+    
