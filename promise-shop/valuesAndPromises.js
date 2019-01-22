d354968478c7643c0894a0ab07c698ac7d6a20fc
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-01-22 09:04:08 +0530

    Added new folder for Assignment 1

--- promise-shop/valuesAndPromises.js
+++ promise-shop/valuesAndPromises.js
@@ -2,7 +2,7 @@ const attachTitle = (firstArg) => {
     return 'DR. '+ firstArg;
 }
 
-const fulfilledPromise = Promise.resolve('MANHATTAN').then(attachTitle)
+const fulfilledPromise = Promise.resolve('MANHATTAN').then(attachTitle);
 fulfilledPromise.then(console.log);
 
 module.exports = fulfilledPromise;
